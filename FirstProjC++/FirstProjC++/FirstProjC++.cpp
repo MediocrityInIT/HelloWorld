4cba681bdcf4c988d87600fbc5bc2c36bb5d6bc4
wizi4884 <114525105+MediocrityInIT@users.noreply.github.com>
2023-02-08 19:07:08 +0300

    Push

---
+++ FirstProjC++/FirstProjC++/FirstProjC++.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Я бездарь, но я смогу!";
+}
---
+++ FirstProjJava/src/main/java/firstproj/firstprojjava/FirstProjJava.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package firstproj.firstprojjava;
+
+/**
+ *
+ * @author wre56
+ */
+public class FirstProjJava {
+
+    public static void main(String[] args) {
+        System.out.println("Я бездарь, но я смогу!");
+    }
+}
